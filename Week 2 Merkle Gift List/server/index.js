d9ebbf8c7baf2e5d66f5620ca8e1dd32e48bf32a
goldnite <goldendevens@gmail.com>
2023-04-20 23:29:53 -0400

    add Week 2 Merkle Gift List

---
+++ Week 2 Merkle Gift List/server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+const MERKLE_ROOT =
+  "0a2cd23c17e5c529c5fa9b3e8ece953727ea62a7510aec37700a4e98587b5174";
+
+app.post("/gift", (req, res) => {
+  const { name, proof } = req.body;
+
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
+    res.send("You got a toy robot!");
+  } else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
