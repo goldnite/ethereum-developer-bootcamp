f4eaab05b0529f781e17051aff2f18f964149fc2
goldnite <goldendevens@gmail.com>
2023-05-08 02:37:07 -0400

    add Week 5 Decentralized Escrow Application

--- Week 5 Decentralized Escrow Application/app/src/App.js
+++ Week 5 Decentralized Escrow Application/app/src/App.js
@@ -29,9 +29,10 @@ function App() {
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    const value = ethers.utils.parseEther(
+      document.getElementById('amount').value
+    );
     const escrowContract = await deploy(signer, arbiter, beneficiary, value);
-
 
     const escrow = {
       address: escrowContract.address,
@@ -55,26 +56,26 @@ function App() {
 
   return (
     <>
-      <div className="contract">
+      <div className='contract'>
         <h1> New Contract </h1>
         <label>
           Arbiter Address
-          <input type="text" id="arbiter" />
+          <input type='text' id='arbiter' />
         </label>
 
         <label>
           Beneficiary Address
-          <input type="text" id="beneficiary" />
+          <input type='text' id='beneficiary' />
         </label>
 
         <label>
-          Deposit Amount (in Wei)
-          <input type="text" id="wei" />
+          Deposit Amount (in Ether)
+          <input type='text' id='amount' />
         </label>
 
         <div
-          className="button"
-          id="deploy"
+          className='button'
+          id='deploy'
           onClick={(e) => {
             e.preventDefault();
 
@@ -85,10 +86,10 @@ function App() {
         </div>
       </div>
 
-      <div className="existing-contracts">
+      <div className='existing-contracts'>
         <h1> Existing Contracts </h1>
 
-        <div id="container">
+        <div id='container'>
           {escrows.map((escrow) => {
             return <Escrow key={escrow.address} {...escrow} />;
           })}
